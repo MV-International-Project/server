cd7017965ce0d1ff4b1fe8bce85f385d501288d1
Adriaan De Saeger <adriaan.de.saeger@student.howest.be>
2021-03-22 11:15:21 +0100

    Add initial project

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const config = require('./config');
+
+const http = require("http");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(errorHandler);
+
+
+/*
+    Error handler
+*/
+function errorHandler(err, req, res, next) {
+    console.log(err);
+    res.status(500)
+    .json({error: err});
+}
+
+/*
+    API requests
+*/
+const router = express.Router()
+app.use('/api', router)
+
+const server = http.createServer(app);
+const port = config.port;
+server.listen(port);
