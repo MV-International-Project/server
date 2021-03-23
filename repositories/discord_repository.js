182661f6dd50e56329e3e60c2252da552add8d04
Adriaan De Saeger <adriaan.de.saeger@student.howest.be>
2021-03-23 11:24:39 +0100

    Merge master into current branch

--- repositories/discord_repository.js
+++ repositories/discord_repository.js
@@ -17,6 +17,25 @@ function fetchWithAccessToken(url, options = {}) {
     return fetch(url, optionsWithToken);
 }
 
+function refreshAccessToken() {
+    const refreshToken = "xHVikocIYsu8ZotjOLOrCTT0vhrSba";
+    fetch(`${ENDPOINT}/oauth2/token`, 
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `client_id=${config.discord.client_id}&client_secret=${config.discord.client_secret}&grant_type=refresh_token&refresh_token=${refreshToken}\&
+        scope=identify%20connections%20gdm.join`
+    })
+    .then(res => res.json())
+    .then(newToken => {
+        console.log(newToken);
+        return resolve(true);
+    })
+    .catch(err => reject(err));
+}
+
 function getUser() {
     return new Promise((resolve, reject) => {
         fetchWithAccessToken(`${ENDPOINT}/users/@me`, 
