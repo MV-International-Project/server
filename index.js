fe0f3edddfe3e2daf5c51cf3fd295619077fde22
Tijl Ryckbosch <tijl.ryckbosch@student.howest.be>
2021-03-23 14:17:24 +0100

    Fix code structure. Move blacklist functions to user-games-repo and make use of controllers

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const config = require('./config');
 
 const { AppError } = require('./errors');
 const userController = require("./controllers/user_controller");
-const user_games_repo = require("./repositories/user_games_repository");
-const userRepo = require("./repositories/user_repository");
+const userGamesController = require("./controllers/user_games_controller");
 
 
 const http = require("http");
@@ -80,7 +79,7 @@ router.post("/users/blacklist/:game_id", (req, res, next) => {
     // TEMP user id
     let userId = 1;
 
-    userRepo.addGameToBlackList(userId, gameId)
+    userGamesController.addGameToBlackList(userId, gameId)
     .then(result => res.status(200).json(result))
     .catch(next);
         
@@ -92,7 +91,7 @@ router.delete("/users/blacklist/:game_id", (req, res, next) => {
     // TEMP user id
     let userId = 1;
 
-    userRepo.removeGameFromBlackList(userId, gameId)
+    userGamesController.removeGameFromBlackList(userId, gameId)
     .then(result => res.status(200).json(result))
     .catch(next);
         
--- repositories/user_repository.js
+++ repositories/user_repository.js
@@ -112,79 +112,12 @@ function updateTokens(userId, discordName, accessToken, refreshToken) {
     });   
 }
 
-function addGameToBlackList(userId, gameId) {
-  return new Promise((resolve, reject) => {
 
-    let connection = mysql.createConnection(config.db);
-
-    connection.connect((error)=>{
-            if(error){
-                reject(error);
-            }
-            else {
-                let sql = "UPDATE user_games SET blacklist = 1 WHERE user_id = ? AND game_id = ? AND blacklist = 0";
-
-                connection.query(sql, [userId, gameId], (err, result) => {
-                    connection.end();
-                    if(err){
-                        reject(err);
-                    }
-                    else {
-                      if (result.affectedRows == 0) {
-                          reject("This game is already on your blacklist!")
-                      }
-                        resolve(true);
-                    }
-                })
-            }
-        });
-  });
-}
-
-
-<<<<<<< HEAD
-
-function removeGameFromBlackList(userId, gameId) {
-  return new Promise((resolve, reject) => {
-
-    let connection = mysql.createConnection(config.db);
-
-    connection.connect((error)=>{
-            if(error){
-                reject(error);
-            }
-            else {
-                let sql = "UPDATE user_games SET blacklist = 0 WHERE user_id = ? AND game_id = ? AND blacklist = 1";
-
-                connection.query(sql, [userId, gameId], (err, result) => {
-                    connection.end();
-                    if(err){
-                        reject(err);
-                    }
-                    else {
-                      if (result.affectedRows == 0) {
-                          reject("This game is not on your blacklist!")
-                      }
-                        resolve(true);
-                    }
-                })
-            }
-        });
-  });
-}
 
 
 
 module.exports={
     addUser,
     getUserFromId,
-    addGameToBlackList,
-    removeGameFromBlackList
-=======
-module.exports= {
-    addUser,
-    getUserFromId,
-    updateTokens,
-    addGameToBlackList
->>>>>>> b72abdbd2a8f5978751b5fa7359d05d6109e923e
+    updateTokens
 };
