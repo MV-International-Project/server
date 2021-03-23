9c63677fd04bd4a099cef6ee224e5454a8d439a0
Tijl Ryckbosch <tijl.ryckbosch@student.howest.be>
2021-03-23 16:00:13 +0100

    Add function in controller respondToPendingMatch that controlls if it accepts or rejects

--- controllers/user_games_controller.js
+++ controllers/user_games_controller.js
@@ -38,7 +38,24 @@ async function removeGameFromBlackList(userId, gameId) {
     return true;
 }
 
+async function respondToPendingMatch(userId, suggestedUserId, accepted) {
+    if(userId == null || suggestedUserId == null || accepted == null) {
+        throw new AppError(400, "Bad request");
+    }
 
+    // Make sure the user exists
+    if(await userRepository.getUserFromId(userId) == null || userRepository.getUserFromId(suggestedUserId) == null) {
+        throw new AppError(404, "User not found.");
+    }
+
+    // Accept match
+    if (accepted == "true") {
+        await userGamesRepository.acceptPendingMatch(userId, suggestedUserId);
+    } else {
+        //await userGamesRepository.rejectPendingMatch(userId, suggestedUserId);
+    }
+    return true;
+}
 
 module.exports = {
     addGameToBlackList,
