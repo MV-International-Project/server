94430c6ab7465f03792f8a540d89279d63871e30
Nathan <nathan.devel@student.howest.be>
2021-03-23 11:06:21 +0100

    Adding data to user_games is possible, but the api call will always return "succeeded".

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const config = require('./config');
 
+const user_games_repo = require("./repositories/user_games_repository");
+
 const http = require("http");
 const express = require("express");
 
@@ -24,6 +26,24 @@ function errorHandler(err, req, res, next) {
 */
 const router = express.Router();
 app.use('/api', router);
+
+router.post("/users/game", (req, res) => {
+    let body = req.body;
+    let user_id = body.user_id;
+    let game_id = body.game_id;
+    let hours_played = body.hours_played;
+    let rank = body.rank;
+    user_games_repo.connectGameToUser(user_id, game_id, hours_played, rank, (err, result) =>{
+
+        if(err){
+            errorHandler(err);
+        }
+        else {
+            res.end("succeeded");
+        }
+    });
+
+});
 
 const server = http.createServer(app);
 const port = config.port;
