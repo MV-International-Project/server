67cc20096f8de8f82ae568e235a047d35847a7e4
Nathan <nathan.devel@student.howest.be>
2021-03-25 14:04:40 +0100

    Added a method to be implemented for all methods that use connections.

---
+++ repositories/connection.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const mysql = require("mysql");
+
+const config = require("../config");
+
+async function createConnection() {
+    return new Promise((resolve, reject) => {
+        let connection = mysql.createConnection(config.db);
+        connection.connect((err)=>{
+            if(err){
+                reject(err)
+            } else {
+                resolve(connection);
+            }
+        })
+    });
+}
+
+module.exports={
+    createConnection
+};
